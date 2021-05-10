a35a68761b513e76f37dca28290b897b7523f0c0
jeebaleepa <eehabarbash@gmail.com>
2021-05-10 18:31:39 +0300

    Added job list skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import JOBS from "./Data";
 function App() {
   return (
     <>
@@ -7,12 +7,25 @@ function App() {
       <main className="container">
         <div className="filters-bar"></div>
         <ul className="jobs-list">
-          <li className="jobs-list-item">
-            Frontend developer  
-          </li>
-          <li className="jobs-list-item">
-            backend developer  
-          </li>
+          {JOBS.map((job) => (
+            <li className="job-list-item">
+              <h5 className="job-company">{job["company"]}</h5>
+              <h3 className="job-position">{job["position"]}</h3>
+              <p className="job-info">
+                <span>{job["postedAt"]}</span>
+                <span>{job["contract"]}</span>
+                <span>{job["location"]}</span>
+              </p>
+              <ul className="job-filters">
+                <li className="filter-tablet">{job["role"]}</li>
+                <li className="filter-tablet">{job["level"]}</li>
+                {job["languages"].map((language) => (
+                  <li className="filter-tablet">{language}</li>
+                ))}
+              </ul>
+              {/* <div className="job-company-logo"></div> */}
+            </li>
+          ))}
         </ul>
       </main>
     </>
